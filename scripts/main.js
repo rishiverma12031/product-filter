e0f507717608bc7c70a2d26a11d18994af818214
vermarishi1203 <vermarishi1203@gmail.com>
2026-05-12 15:12:19 +0530

    add: feature - live search bar

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,8 @@ import { filterProducts, updateFilters, clearAllFilters } from "./filters.js";
 import { renderProducts, renderFilters } from "./render.js";
 
 const productsContainer = document.querySelector(".products__container");
+const searchBar = document.querySelector(".products__searchbar");
+const clearSearchBtn = document.querySelector(".products__button");
 const filtersContainer = document.querySelector(".filters__groups");
 const clearFiltersBtn = document.querySelector(".filters__button");
 
@@ -14,6 +16,32 @@ let filters = loadFilters();
 const filteredProducts = filterProducts(products, filters);
 renderProducts(filteredProducts, productsContainer);
 renderFilters(products, filtersContainer);
+
+searchBar.addEventListener('input', (event) => {
+
+    const input = toTitleCase(event.target.value);
+
+    const filteredProducts = products.filter(product => product.name.includes(input));
+    renderProducts(filteredProducts, productsContainer);
+
+});
+
+clearSearchBtn.addEventListener('click', (event) => {
+
+    searchBar.value = "";
+    renderProducts(products, productsContainer);
+
+});
+
+function toTitleCase(input) {
+
+    return input.trim()
+        .toLowerCase()
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+
+}
 
 filtersContainer.addEventListener('change', (event) => {
 
