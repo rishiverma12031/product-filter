7f34879f11d5c0bed681860163a496f3a3e72e86
vermarishi1203 <vermarishi1203@gmail.com>
2026-05-14 16:24:20 +0530

    add: feature - responsive filterbar

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,18 @@ import { loadFilters, saveFilters } from "./storage.js";
 import { filterProducts, updateFilters, clearSearch, clearAllFilters } from "./filters.js";
 import { renderProducts, renderResultsCount, renderFilters } from "./render.js";
 
+const filtersToggleBtn = document.querySelector(".filters-toggle-button");
+const backdrop = document.querySelector(".backdrop");
+const filtersBar = document.querySelector(".filters");
+
 const productsContainer = document.querySelector(".products__container");
 const searchBar = document.querySelector(".products__searchbar");
 const clearSearchBtn = document.querySelector(".products__button");
+
 const filtersContainer = document.querySelector(".filters__groups");
 const clearFiltersBtn = document.querySelector(".filters__button--clear");
+const closeFiltersBtn = document.querySelector(".filters__button--close");
+
 const resultsCount = document.querySelector(".products__count");
 
 const products = await loadProducts();
@@ -18,6 +25,30 @@ const filteredProducts = filterProducts(products, filters);
 renderProducts(filteredProducts, productsContainer);
 renderResultsCount(products.length, filteredProducts.length, resultsCount);
 renderFilters(products, filtersContainer);
+
+filtersToggleBtn.addEventListener('click', () => {
+
+    filtersBar.classList.add('filters--open');
+    backdrop.classList.remove('hidden');
+    document.body.style.overflow = "hidden";
+
+    closeFiltersBtn.addEventListener('click', () => {
+
+        filtersBar.classList.remove('filters--open');
+        backdrop.classList.add('hidden');
+        document.body.style.overflow = "auto";
+
+    });
+
+    backdrop.addEventListener('click', () => {
+
+        filtersBar.classList.remove('filters--open');
+        backdrop.classList.add('hidden');
+        document.body.style.overflow = "auto";
+
+    });
+
+});
 
 searchBar.addEventListener('input', (event) => {
 
